dba79d02264c7ec5aa28d14f64d99f05a9b73920
Christiaan Versfeld <87129852+ST10091991@users.noreply.github.com>
2024-02-23 12:40:20 +0200

    Version 2 added PROG notes
    Adding updates

---
+++ Modules/PROG (Programming)/Code/Example 1/Example 1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int linecounter = 1;
+                for (int i = 0; i < 100; i++)
+                {
+                    string LineFinal = LineAssemblyright(linecounter);
+                    Console.WriteLine(LineFinal);
+                        LineFinal = null;
+                        LineFinal = LineAssemblyleft(linecounter);
+                    Console.WriteLine(LineFinal);
+                    linecounter++;
+                }
+            }
+            catch
+            {
+
+            }
+            Console.ReadKey();
+        }
+        static string LineAssemblyright(int line)
+        {
+            string defaultline = "\\";
+            string assembled = "";
+            for (int i = 0; i < line; i++)
+            {
+                assembled += defaultline;
+            }
+
+            return assembled;
+        }
+        static string LineAssemblyleft (int line)
+        {
+            string defaultline = "/";
+            string assembled = "";
+            for (int i = 0; i < line; i++)
+            {
+                assembled += defaultline;
+            }
+
+            return assembled;
+        }
+    }
+}
--- Modules/PROG (Programming)/Code/Hello World/First Project/Program.cs
+++ Modules/PROG (Programming)/Code/Hello World/First Project/Program.cs
@@ -3,16 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
-namespace First_Project
+//using comands that specify the resource definition use
+namespace First_Project//project definition (used to refrence the file by other files)
 {
-    internal class Program
+    internal class Program //main argument class
     {
-        static void Main(string[] args)
+        static void Main(string[] args)//main argument channel
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadLine();
-            Console.ReadKey();
+            Console.WriteLine("Hello World!");//console display to "Hello World" however it only displays on a new line
+            Console.ReadLine();//console command to await for user input
+            Console.ReadKey();//console command to read and display arguments
         }
     }
 }
