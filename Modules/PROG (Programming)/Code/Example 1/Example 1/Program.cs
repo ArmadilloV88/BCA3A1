09f5cc1cce69235d755e798aa7b82759d276b9b9
Christiaan Versfeld <87129852+ST10091991@users.noreply.github.com>
2024-02-26 10:05:58 +0200

    prog example 1 updated

--- Modules/PROG (Programming)/Code/Example 1/Example 1/Program.cs
+++ Modules/PROG (Programming)/Code/Example 1/Example 1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,46 +11,365 @@ namespace Example_1
     {
         static void Main(string[] args)
         {
-            try
+            bool Init = Initialization();
+            if(Init)
             {
-                int linecounter = 1;
-                for (int i = 0; i < 100; i++)
-                {
-                    string LineFinal = LineAssemblyright(linecounter);
-                    Console.WriteLine(LineFinal);
-                        LineFinal = null;
-                        LineFinal = LineAssemblyleft(linecounter);
-                    Console.WriteLine(LineFinal);
-                    linecounter++;
-                }
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("        /  /\\     |--\\    /---    \\");
+                Console.WriteLine("       /  /  \\    |----| /     -   \\");
+                Console.WriteLine(" \\    /  /----\\   |--/   |          \\    /");
+                Console.WriteLine("  \\  /  /      \\  |\\     \\    -     \\  /");
+                Console.WriteLine("   \\/  /        \\ | \\     \\---       \\/");
+                Console.WriteLine("---------------------------------------------");
+                Console.WriteLine("                 Arc Ready                   ");
+                Console.WriteLine("---------------------------------------------");
             }
-            catch
+            else
             {
+                 bool made = CreateDirectory("C:\\Users\\ccver\\Desktop\\VC\\BCA3A1\\Modules\\PROG (Programming)\\Code\\Example 1\\Example 1\\Data");
+                 if (made)
+                {
 
+                    CheckDirectory("C:\\Users\\ccver\\Desktop\\VC\\BCA3A1\\Modules\\PROG (Programming)\\Code\\Example 1\\Example 1\\Data");
+                }
+                else
+                {
+
+                }
             }
             Console.ReadKey();
         }
-        static string LineAssemblyright(int line)
+       static bool Initialization()
         {
-            string defaultline = "\\";
-            string assembled = "";
-            for (int i = 0; i < line; i++)
+            bool valid = false;
+            string currentDirectoryPath = "C:\\Users\\ccver\\Desktop\\VC\\BCA3A1\\Modules\\PROG (Programming)\\Code\\Example 1\\Example 1\\Data";
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Looking for root directory");
+            if (Directory.Exists(currentDirectoryPath))
             {
-                assembled += defaultline;
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Directory exists!");
+                Console.ForegroundColor = ConsoleColor.White;
+                valid = true;
+                CheckDirectory(currentDirectoryPath);
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Directory does not exist!");
+                Console.ForegroundColor = ConsoleColor.White;
+                valid = false;
+                CreateDirectory(currentDirectoryPath);
+            }
+            return valid;
+        }
+        static bool CheckDirectoryFiles(string path)
+        {
+            bool valid = false;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Checking Entities folder for UseData.txt file");
+
+            if (File.Exists(path +"\\Entities\\UserData.txt"))
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("File UserData exists in Entities folder");
+                Console.ForegroundColor = ConsoleColor.White;
+                if (File.Exists(path + "\\Entities\\Data.txt"))
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("File Data found in Entities folder");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("Checking Models folder for Data.txt file");
+                    if (File.Exists(path + "\\Models\\Data.txt"))
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("File Data exists in Models folder");
+                        valid = true;
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("File Data does not exists in Models folder");
+                        Console.WriteLine("");
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine("Creating file Data in Models folder");
+                        File.CreateText(path + "\\Models\\Data.txt");
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.WriteLine("Checking Models folder for Data.txt file");
+                        if (File.Exists(path + "\\Models\\Data.txt"))
+                        {
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.WriteLine("File Data found in Models folder");
+                            valid = true;
+                        }
+                        else
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("File Data not found in Entities folder");
+                        }
+
+                    }
+                }
+                else
+                {
+                    File.CreateText(path + "\\Entities\\Data.txt");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("Checking Entities folder for Data.txt file");
+                    if (File.Exists(path + "\\Entities\\Data.txt"))
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("File Data found in Models folder");
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("File Data not found in Models folder");
+                    }
+
+                }
+            }
+            else
+            {
+                File.CreateText(path + "\\Entities\\UserData.txt");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("Checking Entities folder for UserData.txt file");
+                if (File.Exists(path + "\\Entities\\UserData.txt"))
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("File UserData made in Entities folder");
+                    if (File.Exists(path + "\\Models\\Data.txt"))
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("File Data exists in Models folder");
+                        valid = true;
+                    }
+                    else
+                    {
+                        File.CreateText(path + "\\Models\\Data.txt");
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.WriteLine("Checking Models folder for Data.txt file");
+                        if (File.Exists(path + "\\Models\\Data.txt"))
+                        {
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.WriteLine("File Data found in Models folder");
+                            valid = true;
+                        }
+                        else
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("File Data not found in Models folder");
+                        }
+
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("File UserData not made in Entities folder");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("Checking Entities folder for Data.txt file");
+                    if (File.Exists(path + "\\Entities\\Data.txt"))
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("File UserData exists in Entities folder");
+
+                    }
+                    else
+                    {
+                        File.CreateText(path + "\\Entities\\Data.txt");
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.WriteLine("Checking Entities folder for Data.txt file");
+                        if (File.Exists(path + "\\Entities\\Data.txt"))
+                        {
+                            Console.WriteLine("File Data made in Entities folder");
+                            Console.ForegroundColor = ConsoleColor.White;
+                            Console.WriteLine("Checking Entities folder for UserData.txt file");
+                            if (File.Exists(path + "\\Entities\\UserData.txt"))
+                            {
+                                Console.WriteLine("File UserData made in Entities folder");
+                                Console.ForegroundColor = ConsoleColor.White;
+                                Console.WriteLine("Checking Moodels folder for Data.txt file");
+                                if (File.Exists(path + "\\Models\\Data.txt"))
+                                {
+                                    Console.WriteLine("File Data exists in Models folder");
+                                    valid = true;
+                                }
+                                else
+                                {
+                                    File.CreateText(path + "\\Models\\Data.txt");
+                                    Console.ForegroundColor = ConsoleColor.White;
+                                    Console.WriteLine("Checking Moodels folder for Data.txt file");
+                                    if (File.Exists(path + "\\Models\\Data.txt"))
+                                    {
+                                        Console.WriteLine("File Data found in Models folder");
+                                        valid = true;
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine("File Data not found in Models folder");
+                                    }
+
+                                }
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("File Data not made in Entities folder");
+                        }
+
+                    }
+                }
+            }
+                return valid;
+        }
+        static bool CheckDirectory(string directory)
+        {
+            bool valid = false;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Looking for 'Entities' folder");
+            if (Directory.Exists(directory+"\\Entities"))
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Entities folder found");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("Looking for 'Models' folder");
+                if (Directory.Exists(directory + "\\Models"))
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Models folder found");
+                    CheckDirectoryFiles(directory);
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Models folder not found");
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("Creating 'Models' folder");
+                    Directory.CreateDirectory(directory + "\\Models");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("Looking for 'Models' folder");
+                    if (Directory.Exists(directory + "\\Models"))
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("Models folder made");
+                        CheckDirectoryFiles(directory);
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Models folder not made");
+                    }
+                }
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Entities folder not found");
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Creating 'Entities' folder");
+                Directory.CreateDirectory(directory + "\\Entities");
+                if (Directory.Exists(directory + "\\Entities"))
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Entities folder made");
+                    if (Directory.Exists(directory + "\\Models"))
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("Models folder found");
+                        CheckDirectoryFiles(directory);
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Models folder not found");
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine("Creating 'Entities' folder");
+                        Directory.CreateDirectory(directory + "\\Models");
+                        if (Directory.Exists(directory + "\\Models"))
+                        {
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.WriteLine("Models folder made");
+                            CheckDirectoryFiles(directory);
+                        }
+                        else
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("Models folder not made");
+                        }
+                    }
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Entities folder not made");
+                }
+            }
+            return valid;
+        }
+        static bool CreateDirectory(string path)
+        {
+            bool made = false;
+            Directory.CreateDirectory(path);
+            if (Directory.Exists(path))
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Directory created");
+                CheckDirectory(path);
+                made = true;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Directory not created!");
+                made = false;
+            }
+            return made;
+        }
+        /*static void WalkDirectoryTree(System.IO.DirectoryInfo root)
+        {
+            System.IO.FileInfo[] files = null;
+            System.IO.DirectoryInfo[] subDirs = null;
+
+            // First, process all the files directly under this folder 
+            try
+            {
+                files = root.GetFiles("C:\\");
+            }
+            // This is thrown if even one of the files requires permissions greater 
+            // than the application provides. 
+            catch (UnauthorizedAccessException)
+            {
+                // This code just writes out the message and continues to recurse. 
+                // You may decide to do something different here. For example, you 
+                // can try to elevate your privileges and access the file again.
+                Console.WriteLine("Unauthorized access");
             }
 
-            return assembled;
-        }
-        static string LineAssemblyleft (int line)
-        {
-            string defaultline = "/";
-            string assembled = "";
-            for (int i = 0; i < line; i++)
+            catch (System.IO.DirectoryNotFoundException e)
             {
-                assembled += defaultline;
+                Console.WriteLine(e.Message);
             }
 
-            return assembled;
-        }
+            if (files != null)
+            {
+                foreach (System.IO.FileInfo fi in files)
+                {
+                    // In this example, we only access the existing FileInfo object. If we 
+                    // want to open, delete or modify the file, then 
+                    // a try-catch block is required here to handle the case 
+                    // where the file has been deleted since the call to TraverseTree().
+                    Console.WriteLine(fi.FullName);
+                }
+
+                // Now find all the subdirectories under this directory.
+                subDirs = root.GetDirectories();
+
+                foreach (System.IO.DirectoryInfo dirInfo in subDirs)
+                {
+                    // Resursive call for each subdirectory.
+                    WalkDirectoryTree(dirInfo);
+                }
+            }
+        }*/
     }
 }
